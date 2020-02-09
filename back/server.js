132963b01c6607e18b306a7866a565e4c05f5f00
jaewan <stpjw777@gmail.com>
2020-02-09 14:11:06 +0900

    formdata.html

--- back/server.js
+++ back/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+
 const app = express();
 const port = 8001;
+const upload = multer();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); //bodyparse 대신 들어간거
+app.use(express.urlencoded({ extended: false })); //
 app.get("/", (req, res) => res.send("Hello Node.js"));
 app.get("/time", (req, res) => {
   time = Date();
@@ -26,6 +29,14 @@ app.post("/email", (req, res) => {
     res.json(info);
     console.log(email + "비정상접근");
   }
+});
+//app.post(경로, 업로드하는 경로, cb)
+
+app.post("/form", upload.none(), (req, res) => {
+  console.log(req.body);
+  res.send(
+    `정상적으로 데이터를 전송받았습니다. name ${req.body.name}, birth ${req.body.birth}`
+  );
 });
 
 app.listen(port, () => console.log(`server running ... Port : ${port}`));
